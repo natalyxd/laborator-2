6b0812cbdb2356926048c24bffd79ae0d1798a4f
natalyxd <natalia.stefan151103@gmail.com>
2024-10-10 08:04:17 +0200

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,31 +1,28 @@
 document.addEventListener("touchstart", on_touch);
 document.addEventListener("mousedown", on_touch);
 
-var recognition = new webkitSpeechRecognition ();
+var recognition = new webkitSpeechRecognition();
+var recognition_started = false;  // Variabilă globală pentru a verifica dacă recunoașterea a început
 
 recognition.lang = 'en-US';
-function on_touch ()
-    {
-        if (recognition.start){
-            recognition.start();
-            recognition_started = true;
-        }
-    }
 
-function onend ()
-    {
+function on_touch() {
+    if (!recognition_started) {  // Verificăm dacă recunoașterea nu a început deja
+        recognition.start();
+        recognition_started = true;
+    }
+}
+
+function onend() {
     recognition.stop();
     recognition_started = false;
-    }
+}
 
 recognition.onend = onend;
-recognition.onesoundend = onend;
-recognition.onespeechend = onend;
-recognition.oneresult = on_results;
+recognition.onspeechend = onend;
+recognition.onresult = on_results;  // Corectarea numelui evenimentului
 
-function on_results (e)
-    {
-        document.getElementById("text").innerHTML += "Ati rostit cuvantul:    " +
-        e.results [0] [0]. transcript +   ", acuratete: " + e.results [0] [0]
-        .confidence + "<br>"
-    }
+function on_results(e) {
+    document.getElementById("text").innerHTML += "Ati rostit cuvantul: " +
+        e.results[0][0].transcript + ", acuratete: " + e.results[0][0].confidence + "<br>";
+}
